485f8a56cd6a5c501a9661a6871a25c301f41b19
sangadang <sangadang@kms-technology.com>
2026-03-10 16:41:53 +0700

    If we're going to be damned, let's be damned for what we really are.

--- src/main/java/tech/sangdang/camapi/modules/clickstream_processing/app/service/impl/SessionSummaryCreationServiceImpl.java
+++ src/main/java/tech/sangdang/camapi/modules/clickstream_processing/app/service/impl/SessionSummaryCreationServiceImpl.java
@@ -1,14 +1,17 @@
 package tech.sangdang.camapi.modules.clickstream_processing.app.service.impl;
 
+import com.azure.spring.data.cosmos.exception.CosmosAccessException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 import tech.sangdang.camapi.modules.clickstream_processing.app.dto.command.CreateSessionSummaryFromEventCommand;
 import tech.sangdang.camapi.modules.clickstream_processing.app.service.SessionSummaryCreationService;
 import tech.sangdang.camapi.modules.clickstream_processing.domain.SessionSummary;
 import tech.sangdang.camapi.modules.clickstream_processing.domain.SessionSummaryRepository;
 
+import java.time.Duration;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -28,12 +31,16 @@ public class SessionSummaryCreationServiceImpl implements SessionSummaryCreation
                 // if session id found
                 .flatMap(existing -> {
                     existing.setPageCount(existing.getPageCount() + 1);
-                    if(existing.getLastEventTime() == null || existing.getLastEventTime().isBefore(LocalDateTime.ofInstant(Instant.ofEpochMilli(command.getTimestamp()), ZoneOffset.UTC))) {
+                    if (existing.getLastEventTime() == null || existing.getLastEventTime().isBefore(LocalDateTime.ofInstant(Instant.ofEpochMilli(command.getTimestamp()), ZoneOffset.UTC))) {
                         existing.setLastEventTime(LocalDateTime.ofInstant(Instant.ofEpochMilli(command.getTimestamp()), ZoneOffset.UTC));
                         existing.setDurationSec(calculateDurationSec(existing.getFirstEventTime(), existing.getLastEventTime()));
                     }
                     return sessionSummaryRepository.save(existing);
                 })
+                .retryWhen(Retry
+                        .backoff(6, Duration.ofMillis(100))
+                        .filter(e -> e instanceof CosmosAccessException)
+                )
                 // if session id not found
                 .switchIfEmpty(Mono.defer(() -> {
                     SessionSummary sessionSummary = SessionSummary.builder()
@@ -50,7 +57,7 @@ public class SessionSummaryCreationServiceImpl implements SessionSummaryCreation
                 }))
                 .then();
     }
-    
+
     private Integer calculateDurationSec(LocalDateTime firstEventTime, LocalDateTime lastEventTime) {
         return (int) (lastEventTime.toEpochSecond(ZoneOffset.UTC) - firstEventTime.toEpochSecond(ZoneOffset.UTC));
     }
