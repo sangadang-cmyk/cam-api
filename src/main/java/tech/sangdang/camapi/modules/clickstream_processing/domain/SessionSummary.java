72ab6cbe4223bd1f9e4f436273b63dbd3f073cf8
sangadang <sangadang@kms-technology.com>
2026-03-10 16:32:41 +0700

    Seize the time... Live now! Make now always the most precious time. Now will never come again.

--- src/main/java/tech/sangdang/camapi/modules/clickstream_processing/domain/SessionSummary.java
+++ src/main/java/tech/sangdang/camapi/modules/clickstream_processing/domain/SessionSummary.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Version;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -26,4 +27,6 @@ public class SessionSummary {
     private LocalDateTime lastEventTime;
     @Builder.Default
     private LocalDateTime processedAt = LocalDateTime.now();
+    @Version
+    private String _etag;
 }
