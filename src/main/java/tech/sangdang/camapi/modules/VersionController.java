6972f2989b89f8acaa019ca0e81d4e243d5d3250
sangadang <sangadang@kms-technology.com>
2026-03-11 14:45:11 +0700

    Weird is part of the job +2

---
+++ src/main/java/tech/sangdang/camapi/modules/VersionController.java
@@ -0,0 +1,15 @@
+package tech.sangdang.camapi.modules;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/version")
+@RestController
+public class VersionController {
+    @GetMapping()
+    public String getVersion() {
+        int version = 1;
+        return Integer.toString(version);
+    }
+}
