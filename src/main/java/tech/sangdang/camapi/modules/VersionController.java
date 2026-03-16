65dc413f8b835da7f05432c2d336f62e8b5edcf9
sangadang <sangadang@kms-technology.com>
2026-03-16 11:08:00 +0700

    There's surviving, and then there's living

--- src/main/java/tech/sangdang/camapi/modules/VersionController.java
+++ src/main/java/tech/sangdang/camapi/modules/VersionController.java
@@ -1,15 +1,28 @@
 package tech.sangdang.camapi.modules;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+
+@Slf4j
 @RequestMapping("/version")
 @RestController
 public class VersionController {
     @GetMapping()
     public String getVersion() {
         int version = 2;
+        log.info("version: {}", version);  
         return Integer.toString(version);
+    }
+    
+    @GetMapping("/slow")
+    public String getSlowVersion() throws InterruptedException {
+        Integer version = 2;
+        log.info("version: {}", version);
+        Thread.sleep(Duration.ofSeconds(5));
+        return version.toString();
     }
 }
